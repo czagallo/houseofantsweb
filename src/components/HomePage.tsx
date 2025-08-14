c044bc709e1184e2c8fbb7ef21209332683672cf
czagallo <michaeljezt@gmail.com>
2025-08-14 13:58:54 +0200

    new video link added

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -52,7 +52,7 @@ export default function HomePage() {
               </div>
             )}
             <iframe
-              src="https://www.youtube.com/embed/SVeMHqWQ8og?autoplay=0&mute=0"
+              src="https://www.youtube.com/embed/XjpD10cdUsA?autoplay=0&mute=0"
               title="House of Ants Live Stream"
               className="w-full h-full"
               allowFullScreen
